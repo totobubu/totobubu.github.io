3392fc25cf0098866b5bf249db92bf605e0b33a5
맑은 <160807468+malkeun@users.noreply.github.com>
2025-07-24 16:23:18 +0900

    요일 색상

--- stock/src/layouts/AppSidebar.vue
+++ stock/src/layouts/AppSidebar.vue
@@ -133,6 +133,16 @@ const getGroupSeverity = (group) => {
       return "success";
     case "D":
       return "info";
+    case "월요일":
+      return "mon";
+    case "화요일":
+      return "tue";
+    case "수요일":
+      return "wed";
+    case "목요일":
+      return "thu";
+    case "금요일":
+      return "fri";
     default:
       return "secondary";
   }
