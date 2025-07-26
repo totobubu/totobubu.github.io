f5acfc4ecba803f52d04400345892b6f209226ed
totobubu <totobubu.lab@gmail.com>
2025-07-27 00:59:15 +0900

    Update main.js

--- stock/src/main.js
+++ stock/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 
 import PrimeVue from 'primevue/config';
+import { definePreset } from '@primeuix/themes';
 import Lara from "@primeuix/themes/lara";
 
 import './styles/style.scss';
@@ -17,12 +18,80 @@ ChartJS.register(
     ChartDataLabels
 );
 
+const MyPreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{zinc.50}',
+            100: '{zinc.100}',
+            200: '{zinc.200}',
+            300: '{zinc.300}',
+            400: '{zinc.400}',
+            500: '{zinc.500}',
+            600: '{zinc.600}',
+            700: '{zinc.700}',
+            800: '{zinc.800}',
+            900: '{zinc.900}',
+            950: '{zinc.950}'
+        },
+        colorScheme: {
+            dark: {
+                primary: {
+                    color: '{zinc.50}',
+                    inverseColor: '{zinc.950}',
+                    hoverColor: '{zinc.100}',
+                    activeColor: '{zinc.200}'
+                },
+                highlight: {
+                    background: 'rgba(250, 250, 250, .16)',
+                    focusBackground: 'rgba(250, 250, 250, .24)',
+                    color: 'rgba(255,255,255,.87)',
+                    focusColor: 'rgba(255,255,255,.87)'
+                },
+                formField: {
+                    hoverBorderColor: '{primary.color}'
+                }
+            }
+        },
+        focusRing: {
+            width: '2px',
+            style: 'dashed',
+            color: '{primary.color}',
+            offset: '1px'
+        }
+    },
+    components: {
+        card: {
+            colorScheme: {
+                light: {
+                    root: {
+                        background: '{surface.0}',
+                        color: '{surface.700}'
+                    },
+                    subtitle: {
+                        color: '{surface.500}'
+                    }
+                },
+                dark: {
+                    root: {
+                        background: '{surface.900}',
+                        color: '{surface.0}'
+                    },
+                    subtitle: {
+                        color: '{surface.400}'
+                    }
+                }
+            }
+        }
+    }
+});
+
+
 const app = createApp(App);
 
 app.use(router);
 app.use(PrimeVue, {
     theme: {
-        preset: Lara,
+        preset: MyPreset,
         options: {
             darkModeSelector: ".p-dark",
         }
