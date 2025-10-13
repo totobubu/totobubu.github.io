bdf49ad71b392ba5944c5cd93bb7dbf7671e7f8b
맑은 <160807468+malkeun@users.noreply.github.com>
2025-10-13 15:05:02 +0900

    Update HomeView.vue

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -13,55 +13,57 @@
 </script>
 
 <template>
-    <header class="t-home-header">
-        <p class="logo">
-            <img src="/src/assets/apple-touch-icon.png" alt="" />
-        </p>
-        <h1 class="h1">배당모아 Div Grow</h1>
-        <h2>파이어족을 위한 배당 포트폴리오 추적 플랫폼</h2>
-        <p class="desc">
-            ETF부터 개별주까지, 나만의 배당금 성장 기록과 관리. 지금 바로 배당의
-            여정을 시작하세요.
-        </p>
-    </header>
+    <main id="t-home">
+        <header class="t-home-header">
+            <p class="logo">
+                <img src="/src/assets/apple-touch-icon.png" alt="" />
+            </p>
+            <h1 class="h1">배당모아 Div Grow</h1>
+            <h2>파이어족을 위한 배당 포트폴리오 추적 플랫폼</h2>
+            <p class="desc">
+                ETF부터 개별주까지, 나만의 배당금 성장 기록과 관리. 지금 바로
+                배당의 여정을 시작하세요.
+            </p>
+        </header>
 
-    <article class="t-home-link">
-        <Button
-            label="배당달력"
-            icon="pi pi-calendar"
-            severity="secondary"
-            rounded
-            size="large"
-            @click="router.push('/calendar')" />
-        <Button
-            label="백테스터"
-            icon="pi pi-history"
-            severity="secondary"
-            rounded
-            size="large"
-            @click="router.push('/backtester')" />
-        <Button
-            v-if="!user"
-            label="로그인"
-            icon="pi pi-user"
-            severity="secondary"
-            rounded
-            size="large"
-            @click="router.push('/login')" />
-        <Button
-            v-else
-            label="마이페이지"
-            icon="pi pi-user"
-            severity="secondary"
-            rounded
-            size="large"
-            @click="router.push('/mypage')" />
-        <Button
-            label="문의하기"
-            icon="pi pi-envelope"
-            severity="secondary"
-            rounded
-            size="large"
-            @click="router.push('/contact')" />
-    </article>
+        <article class="t-home-link">
+            <Button
+                label="배당달력"
+                icon="pi pi-calendar"
+                severity="secondary"
+                rounded
+                size="large"
+                @click="router.push('/calendar')" />
+            <Button
+                label="백테스터"
+                icon="pi pi-history"
+                severity="secondary"
+                rounded
+                size="large"
+                @click="router.push('/backtester')" />
+            <Button
+                v-if="!user"
+                label="로그인"
+                icon="pi pi-user"
+                severity="secondary"
+                rounded
+                size="large"
+                @click="router.push('/login')" />
+            <Button
+                v-else
+                label="마이페이지"
+                icon="pi pi-user"
+                severity="secondary"
+                rounded
+                size="large"
+                @click="router.push('/mypage')" />
+            <Button
+                label="문의하기"
+                icon="pi pi-envelope"
+                severity="secondary"
+                rounded
+                size="large"
+                @click="router.push('/contact')" />
+        </article>
+    </main>
 </template>
