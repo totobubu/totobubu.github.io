8162218eb3406d116eac510f0ff2123a5bc8437b
totobubu <totobubu.lab@gmail.com>
2025-11-16 12:40:49 +0900

    Update useStockData.js

--- src/composables/useStockData.js
+++ src/composables/useStockData.js
@@ -218,6 +218,10 @@ export function useStockData() {
                                 item.amountFixed !== undefined
                                     ? item.amountFixed
                                     : item.amount,
+                            // 분할 전 원금 및 조정내역을 표기용으로 전달
+                            amountOriginal: item.amountOriginal,
+                            amountSplitAdjustments:
+                                item.amountSplitAdjustments,
                             배당률: item.yield,
                             전일종가: formatPriceField(prevDayData?.close),
                             당일시가: formatPriceField(item.open),
